80e359ed2c34ba02287bfe3c1547be64152ebd84
NikhilBhawar11 <bhawarnikhil4@gmail.com>
2026-03-08 12:13:55 +0530

    ML placement predictor web app

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 import pickle
 
 # Load dataset
-data = pd.read_csv("placementdata.csv")
+data = pd.read_csv("data/placementdata.csv")
 
 # Encode categorical columns
 le1 = LabelEncoder()
@@ -26,6 +26,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 # Train model
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
+
+from sklearn.metrics import accuracy_score
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Model Accuracy:", accuracy)
 
 # Save model
 pickle.dump(model, open("model.pkl", "wb"))
